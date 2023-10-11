ea03b9d9b3bdb90a8e492c41156f4c10c0398b7b
Kep24 <114787107+Kep24@users.noreply.github.com>
2023-10-11 17:04:52 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php include 'includes/session.php'; ?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+	<?php include 'includes/navbar.php'; ?>
+	 
+	  <div class="content-wrapper">
+	    <div class="container">
+
+
+	      <section class="content">
+	        <div class="row">
+	        	<div class="col-sm-9">
+	        		<?php
+	        			if(isset($_SESSION['error'])){
+	        				echo "
+	        					<div class='alert alert-danger'>
+	        						".$_SESSION['error']."
+	        					</div>
+	        				";
+	        				unset($_SESSION['error']);
+	        			}
+	        		?>
+	        		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		                <ol class="carousel-indicators">
+		                  <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+		                  <li data-target="#carousel-example-generic" data-slide-to="1" class=""></li>
+		                  <li data-target="#carousel-example-generic" data-slide-to="2" class=""></li>
+		                </ol>
+		                <div class="carousel-inner">
+		                  <div class="item active">
+		                    <img src="images/picture1.png" alt="First slide">
+		                  </div>
+		                  <div class="item">
+		                    <img src="images/picture2.png" alt="Second slide">
+		                  </div>
+		                  <div class="item">
+		                    <img src="images/picture3.png" alt="Third slide">
+		                  </div>
+		                </div>
+		                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+		                  <span class="fa fa-angle-left"></span>
+		                </a>
+		                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+		                  <span class="fa fa-angle-right"></span>
+		                </a>
+		            </div>
+		
+		       		
+	        	</div>
+	        	<div class="col-sm-3">
+	        		<?php include 'includes/sidebar.php'; ?>
+	        	</div>
+	        </div>
+	      </section>
+	     
+	    </div>
+	  </div>
+  
+  	
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+</body>
+</html>
