b2651c56238b0ade2c09c5f5867f445764c4f357
Yessica Quezada <ysq2@njit.edu>
2023-10-11 15:00:47 -0400

    SQL

--- DataBase/MySQL/001_create_UserTable.sql
+++ DataBase/MySQL/001_create_UserTable.sql
@@ -1,11 +1,11 @@
 CREATE TABLE 'Users' (
 	'ID'            INT,
 	'UserNames'     VARCHAR(20) NOT NULL, 
-	'PassWords'     VARCHAR(20) NOT NULL, 
-	'Email' 	VARCHAR(20) NOT NULL, 
+	'Passwords'     BINARY(60) NOT NULL, 
+	'Email' 	VARCHAR(40) NOT NULL, 
 	'AccessLevel'   INT, 
 
 	PRIMARY KEY ('ID'),
-	UNIQUE ('Email')
+	UNIQUE('Email'),
 
 ); 
