5cedb2472d6e0dda64d307846944d2af1aa78fe2
Red <73609416+MrRedstone54@users.noreply.github.com>
2023-10-18 16:20:46 -0400

    Added test of logging system: Database

--- DataBase/UserRegistration.php
+++ DataBase/UserRegistration.php
@@ -1,6 +1,12 @@
 //DataBase Connection
 
 <?php 
+require_once('path.inc');
+require_once('get_host_info.inc');
+require_once('rabbitMQLib.inc');
+$client_log = new rabbitMQClient("logging.ini", "Logging");
+
+
 
 	$dbhost = "localhost"; 
 	$dbuser = "yessica"; 
@@ -14,6 +20,8 @@
 	//Verifying connection
 	if (!$conn) 
 	{
+	$log = "Connection Failed: " . mysqli_connect_error();
+	$client_log->publish($log);
 		die("Connection Failed: " . mysqli_connect_error()); 
 	}
 
