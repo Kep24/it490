2c566bd6b82c872686fc9f57b6dcb2187d21e047
Red <73609416+MrRedstone54@users.noreply.github.com>
2023-11-15 15:50:58 -0500

    re-implimenting hashing, needs testing. Also needs registration

--- DataBase/UserLogic.php
+++ DataBase/UserLogic.php
@@ -1,22 +1,54 @@
 #!/usr/bin/php
 
 <?php 
+require_once('rabbitMQLib.inc');
+require_once('get_host_info.inc');
+require_once('path.inc');
 
 //Database conenction setup 
-       // echo password_hash("NJIT490", PASSWORD_DEFAULT);
+ $client_log = new rabbitMQClient("testRabbitMQ.ini", "Logging");
+	$dbuser = "red"; 
+	$dbpass = "490Pass"; 
+
+	//Creating connection with Database
+	try{
+	$conn = new PDO('mysql:dbname=it490;host=localhost', $dbuser, $dbpass); 
+	}catch (PDOException $e){
+	$log = "Error: ". $e->getMessage();
+	print $log;
+	$client_log->publish($log);
+	}
 
 
 function isInputValid($username, $email, $password) 
 {
-	//Implement input validation logic
-	//Return true if input is valid, faster otherwise 
-
+	try{
+	$stmt = $conn->prepare("SELECT UserName, Email, PassWord FROM Users WHERE UserNames = :username && Email=:email Passwords = :password");
+		$stmt->bindParam(':username', $username);
+		$stmt->bindParam(':email', $email);
+		$stmt->bindParam(':password', $hashedPassword);
+		$stmt->execute();
+		}catch	(PDOException $e){
+	$log = "Error: ". $e->getMessage();
+	print $log;
+	$client_log->publish($log);
+	}
 } 
 
-function createUser($username, $email, $hashedPassword) 
+function createUser($username, $email, $password) 
 {
-	//Insert the new user into the database
-	//Return true if the user is successfully inserted, false otherwise
+	$hashedPassword = password_hash($password, PASSWORD_DEFAULT)
+	try{
+	$stmt = $conn->prepare("INSERT UserName, Email, PassWord FROM Users WHERE UserNames = :username && Email=:email Passwords = :password");
+	$stmt->bindParam(':username', $username);
+	$stmt->bindParam(':email', $email);
+	$stmt->bindParam(':password', $hashedPassword);
+	$stmt->execute();
+	}catch (PDOException $e){
+	$log = "Error: ". $e->getMessage();
+	print $log;
+	$client_log->publish($log);
+	}
 } 
 ?>
 
