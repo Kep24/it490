822cc066c0f64634077f77bf5eb796020f418914
Red <73609416+MrRedstone54@users.noreply.github.com>
2023-11-17 21:20:35 -0500

    adding extra logging recievers for seperate machines

---
+++ Logging/log_reciever_Front.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import pika
+from pika import DeliveryMode
+from pika.exchange_type import ExchangeType
+
+credentials = pika.PlainCredentials#('', '') Put in your rabbitMQ user/pass here
+parameters = pika.ConnectionParameters('10.147.20.15', 5672, 'testHost', credentials)
+connection = pika.BlockingConnection(parameters)
+
+channel=connection.channel()
+channel.exchange_declare(exchange='logs', exchange_type='fanout',
+ passive=False, durable=True, auto_delete=False)
+ 
+queue = channel.queue_declare(queue='loggingFront', Durable=True)
+queue_name = queue.method.queue
+
+channel.queue_bind(exchange="logs", queue=queue_name)
+
+def callback(ch, method, properties, body):
+	print(f" [x] {body}") 
+ 
+channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+channel.start_consuming()
+
+
