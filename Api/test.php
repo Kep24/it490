9dbdf0b8b16be93f531be7157d042c57a9f10dce
CFountain23 <90641672+CFountain23@users.noreply.github.com>
2023-12-01 16:32:03 -0500

    testing

---
+++ Api/test.php
@@ -0,0 +1,6 @@
+<?php
+    // $path = " Desktop\Fall '23\IT490 tings\it490\Api";
+    // exec("cd".$path. " && node test.js", $out, $err);
+    exec("node server.js");
+    echo "works".PHP_EOL;
+?>
