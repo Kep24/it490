7077bbce496d9ba18615cfbf43508f2466a73eae
CFountain23 <cnf4@njit.edu>
2023-12-20 15:15:24 -0500

    updated pathing in test.php

--- Api/test.php
+++ Api/test.php
@@ -1,7 +1,7 @@
 <?php
     // $path = " Desktop\Fall '23\IT490 tings\it490\Api";
     // exec("cd".$path. " && node test.js", $out, $err);
-    exec("cd .\Api  && node genSearch.js", $output);
+    exec("cd \Api  && node genSearch.js", $output);
     $str = "";
     // foreach ($output as $x){
     //     echo"$x ";
@@ -9,6 +9,7 @@
     foreach ($output as $x){
         $str .="$x ";
     }
+    echo $str;
     return $str;
     
 ?>
