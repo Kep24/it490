d36466c924ec3a9cb03d577d236bf3ed4311f28b
Carlton Fountain <cnf4@njit.edu>
2023-12-20 14:41:39 -0500

    trying to send to db

--- Api/test.php
+++ Api/test.php
@@ -1,11 +1,15 @@
 <?php
     // $path = " Desktop\Fall '23\IT490 tings\it490\Api";
     // exec("cd".$path. " && node test.js", $out, $err);
-    exec("cd .\Api");
     exec("cd .\Api  && node genSearch.js", $output);
+    $str = "";
+    // foreach ($output as $x){
+    //     echo"$x ";
+    // }
     foreach ($output as $x){
-        echo"$x \n";
+        $str .="$x ";
     }
+    return $str;
     
 ?>
 
