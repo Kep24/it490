a1a4124ca311a373da5c511f1a062838eab04b14
Kep24 <114787107+Kep24@users.noreply.github.com>
2023-12-20 18:27:27 -0500

    Create add_to_team.php

---
+++ add_to_team.php
@@ -0,0 +1,8 @@
+<?php
+// Your database connection code here
+
+$pokemon_id = $_GET['id']; // Assuming you pass the Pokémon ID through the URL
+$team_name = $_POST['team_name'];
+
+// Check if the Pokémon and team exist in the database, then insert into the teams table
+?>
