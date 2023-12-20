8ad3bb780f247f0284cfa360eaba6937d2a28b97
Red <73609416+MrRedstone54@users.noreply.github.com>
2023-12-19 23:22:25 -0500

    modified logger to work better on other machines

--- Logging/log_recieverAdmin.py
+++ Logging/log_recieverAdmin.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 import pika
 from datetime import datetime
-from pika import DeliveryMode
-from pika.exchange_type import ExchangeType
-
 now = datetime.now()
 
-credentials = pika.PlainCredentials('admin', '490Pass') #Put in your rabbitMQ user/pass here
-parameters = pika.ConnectionParameters('10.147.20.15', 5672, 'testHost', credentials)
+credentials = pika.PlainCredentials('RedAdmin', '490Pass') #Put in your rabbitMQ user/pass here
+parameters = pika.ConnectionParameters('10.147.20.57', 5672, 'testHost', credentials)
 connection = pika.BlockingConnection(parameters)
 
 channel=connection.channel()
