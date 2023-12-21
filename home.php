5fa4504b10e46c726df748b5d0e545c24e07ce41
CFountain23 <90641672+CFountain23@users.noreply.github.com>
2023-12-21 11:51:49 -0500

    updated styles for homepage

--- home.php
+++ home.php
@@ -1,25 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="Assets/css/home.css">
 </head>
+
 <body>
+    <div class="container-fluid bg-image" style="background-image: url('Assets/images/background.jpg'); height: 100vh;">
+        <div class="bg-image">
+            <div class="row">
+                <div class="col"></div>
+                <div class="col jumbotron text-center">
+                    <h1>Welcome to the Pokemon Portal!</h1>
+                </div>
+                <div class="col"></div>
+            </div>
+            <div class="row" >
+                <div class="col"></div>
+                <div class="col-3" id="box">
+                    <p>Explore the Pokemon world:
+                    <br>
+                    <a href="pokesearch.php">Pokemon Team Builder</a>
+                    
+                    </p>
+                        
+                </div>
+                <div class="col"></div>
+            </div>
+            <div class="row"></div>
+        </div>
 
-    <h1>Welcome to the Pokemon Portal!</h1>
 
-    <p>Explore the Pokemon world:</p>
+    </div>
 
-    <ul>
-<<<<<<< HEAD
-        <li><a href="pokesearch.php">Pokemon Search</a></li>
-=======
-        <li><a href="pokesearch.php">Pokemon Team Builder</a></li>
->>>>>>> 61fd0eb4c0d7b3381a2c37d66ef8cd42b338ea18
-        <!-- Add more links if needed -->
-    </ul>
+
 
 </body>
+
 </html>
