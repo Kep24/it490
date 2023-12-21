f0cecbecbd2934e3af964210d22d70d3e3db8f8d
Kep24 <114787107+Kep24@users.noreply.github.com>
2023-12-21 09:35:28 -0500

    Update conn.php

--- conn.php
+++ conn.php
@@ -4,7 +4,7 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+$client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
 
 $request = array();
 $request['type'] = "login";
@@ -12,9 +12,16 @@ $request['username'] = $_POST["user"];
 $request['password'] = $_POST["password"];
 $response = $client->send_request($request);
 
-echo "client received response: ".PHP_EOL;
-print_r($response);echo "\n\n";
+echo "client received response: " . PHP_EOL;
+print_r($response);
+echo "\n\n";
 
-echo $argv[6]." END".PHP_EOL;
- 
+// Check if the login was successful (customize this condition based on your response structure)
+if (isset($response['allow']) && $response['allow'] == true) {
+    // Redirect to home.php
+    header("Location: home.php");
+    exit; // Make sure to exit after sending the header to prevent further script execution
+}
+
+echo $argv[6] . " END" . PHP_EOL;
 ?>
