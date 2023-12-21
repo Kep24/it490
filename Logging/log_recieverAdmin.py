d0aeaa1b5cc2fc0702d6c0a80a26a4f817e7508a
Red <73609416+MrRedstone54@users.noreply.github.com>
2023-12-20 20:18:17 -0500

    cleaning up git junk in code

--- Logging/log_recieverAdmin.py
+++ Logging/log_recieverAdmin.py
@@ -1,29 +1,15 @@
 #!/usr/bin/env python
 import pika
-<<<<<<< HEAD
-from pika import DeliveryMode
-from pika.exchange_type import ExchangeType
-
-credentials = pika.PlainCredentials('admin', '490Pass') #Put in your rabbitMQ user/pass here
-parameters = pika.ConnectionParameters('10.147.20.15', 5672, 'testHost', credentials)
-=======
-from datetime import datetime
-now = datetime.now()
 
 credentials = pika.PlainCredentials('RedAdmin', '490Pass') #Put in your rabbitMQ user/pass here
 parameters = pika.ConnectionParameters('10.147.20.57', 5672, 'testHost', credentials)
->>>>>>> 8ad3bb780f247f0284cfa360eaba6937d2a28b97
 connection = pika.BlockingConnection(parameters)
 
 channel=connection.channel()
 channel.exchange_declare(exchange='logs', exchange_type='fanout',
  passive=False, durable=True, auto_delete=False)
  
-<<<<<<< HEAD
-queue = channel.queue_declare(queue='')
-=======
 queue = channel.queue_declare(queue='logging', durable=True)
->>>>>>> 8ad3bb780f247f0284cfa360eaba6937d2a28b97
 queue_name = queue.method.queue
 
 channel.queue_bind(exchange="logs", queue=queue_name)
@@ -33,5 +19,3 @@ def callback(ch, method, properties, body):
  
 channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
 channel.start_consuming()
-
-
