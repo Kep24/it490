72299040563b0fbbad7c7929e0d2713c14300ba8
kep <kep24@njit.edu>
2023-12-21 00:49:21 -0500

    conreg fix

--- conreg.php
+++ conreg.php
@@ -4,18 +4,31 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-$client = new rabbitMQClient("testRabbitMQ.ini","testServer");
+try {
+    $client = new rabbitMQClient("testRabbitMQ.ini", "testServer");
+
+    if (!$client) {
+        throw new Exception("Failed to creat RabbitMQ client");
+
+    }
+
 
 $request = array();
 $request['type'] = "registration";
 $request['username'] = $_POST["user"];
 $request['password'] = $_POST["password"];
 $request['email'] = $_POST["email"];
-$response = $client->send_request($request);
+
+$responce = $client->send_request($request);
 
 echo "client received response: ".PHP_EOL;
-print_r($response);echo "\n\n";
+print_r($request);echo "\n\n";
 
 echo $argv[6]." END".PHP_EOL;
- 
+
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage() . PHP_EOL;
+
+}
+
 ?>
