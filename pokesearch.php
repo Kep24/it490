b27fce0f2edeee29f2bdba8e0bca1e113e381fa7
CFountain23 <90641672+CFountain23@users.noreply.github.com>
2023-12-21 12:45:08 -0500

    fixing styles

--- pokesearch.php
+++ pokesearch.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -7,31 +8,53 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="Assets/css/pokesearch.css">
 </head>
+
 <body>
-<div class="container-fluid bg-image" style="background-image: url('Assets/images/background.jpg'); height: 100vh;">
-    <h1>Pokémon Team Builder</h1>
+    <div class="container-fluid bg-image" style="background-image: url('Assets/images/background.jpg'); height: 100vh;">
+        <div class="row">
+            <div class="col"></div>
+            <div class="col">
+                <h1>Pokémon Team Builder</h1>
+            </div>
+            <div class="col"></div>
+        </div>
+        <div class="row"><!-- Search Form -->
+            <div class="col"></div>
+            <div class="col">
+                <form action="search.php" method="get">
+                    <label for="name">Name:</label>
+                    <input type="text" name="name" id="name" placeholder="Enter Pokémon Name"><br>
 
-    <!-- Search Form -->
-    <form action="search.php" method="get">
-        <label for="name">Name:</label>
-        <input type="text" name="name" id="name" placeholder="Enter Pokémon Name">
+                    <label for="generation">Generation:</label>
+                    <input type="text" name="generation" id="generation" placeholder="Enter Generation"><br>
 
-        <label for="generation">Generation:</label>
-        <input type="text" name="generation" id="generation" placeholder="Enter Generation">
+                    <label for="type">Type:</label>
+                    <input type="text" name="type" id="type" placeholder="Enter Pokémon Type"><br>
 
-        <label for="type">Type:</label>
-        <input type="text" name="type" id="type" placeholder="Enter Pokémon Type">
+                    <input type="submit" value="Search">
+                </form>
+            </div>
+            <div class="col"></div>
 
-        <input type="submit" value="Search">
-    </form>
+        </div>
+        <div class="row"><!-- Results Display and Add to Team Form -->
+            <?php include 'display_results.php'; ?>
+        </div>
+        <div class="row">
+            <div class="col"></div>
+            <div class="col-2" id="box">
+                <a href="home.php">Return to Home Page</a>
+                <!-- Add more links if needed -->
+            </div>
+            <div class="col"></div>
+        </div>
 
-    <!-- Results Display and Add to Team Form -->
-    <?php include 'display_results.php'; ?>
 
-    <ul>
-        <li><a href="home.php">Return to Home Page</a></li>
-        <!-- Add more links if needed -->
-    </ul>
-</div>
+
+
+
+
+    </div>
 </body>
+
 </html>
