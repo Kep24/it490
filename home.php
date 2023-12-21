bcd02872f61b920ce7867841eb71bf9a75e839dc
Kep24 <114787107+Kep24@users.noreply.github.com>
2023-12-20 20:02:11 -0500

    Create home.php

---
+++ home.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+
+    <h1>Welcome to the Pokemon Portal!</h1>
+
+    <p>Explore the Pokemon world:</p>
+
+    <ul>
+        <li><a href="pokemon_search.php">Pokemon Search</a></li>
+        <!-- Add more links if needed -->
+    </ul>
+
+</body>
+</html>
